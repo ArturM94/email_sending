5a79133a683a2320299bd3c1da8bab8ed1568f72
Artur Manukian <manukian.artur.94@gmail.com>
2019-01-19 22:39:01 +0200

    Add sendmail function.

--- SMTP_SSL.py
+++ SMTP_SSL.py
@@ -20,4 +20,4 @@ context = ssl.create_default_context()
 
 with smtplib.SMTP_SSL('smtp.gmail.com', port, context=context) as server:
     server.login('manukian.dev@gmail.com', password)
-    # Sending email
+    server.sendmail(sender_email, receiver_email, message)
