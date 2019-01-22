6cb7fe889819bb39d24fa4b10dba99817f5edff0
Artur Manukian <manukian.artur.94@gmail.com>
2019-01-22 12:43:51 +0200

    Add html content.

--- SMTP_SSL.py
+++ SMTP_SSL.py
@@ -1,16 +1,42 @@
 import smtplib
 import ssl
 
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+
 port = 465  # SSL
 # TODO Implement getpass
 password = input('Type your password and press Enter: ')
 
 sender_email = 'manukian.dev@gmail.com'
 receiver_email = 'manukian.artur.94@gmail.com'
-message = '''\
-Subject: Hello
 
-This message is sent from Python.'''
+message = MIMEMultipart('alternative')
+message['Subject'] = 'multipart test'
+message['From'] = sender_email
+message['To'] = receiver_email
+
+text = '''\
+Hi,
+How are you?
+Read Python documentation:
+https://docs.python.org/3'''
+
+html = '''\
+<html>
+  <body>
+    <p>Hi,<br>
+       How are you?<br>
+       Read <a href='https://docs.python.org/3'>Python documentation</a>.
+  </body>
+</html>
+'''
+
+part1 = MIMEText(text, 'plain')
+part2 = MIMEText(html, 'html')
+
+message.attach(part1)
+message.attach(part2)
 
 # Secure SSL connection
 # load the system’s trusted CA certificates,
@@ -20,4 +46,4 @@ context = ssl.create_default_context()
 
 with smtplib.SMTP_SSL('smtp.gmail.com', port, context=context) as server:
     server.login('manukian.dev@gmail.com', password)
-    server.sendmail(sender_email, receiver_email, message)
+    server.sendmail(sender_email, receiver_email, message.as_string())
